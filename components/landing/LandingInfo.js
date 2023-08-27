05fca38f1ce28791f47f80dcc5f12b05c2d6462f
Chaffexd <shanechaffe@gmail.com>
2023-08-27 16:46:11 +0100

    push

--- components/landing/LandingInfo.js
+++ components/landing/LandingInfo.js
@@ -1,5 +1,6 @@
 import GithubIcon from "../icons/GithubIcon";
 import LinkedInIcon from "../icons/LinkedInIcon";
+import Typewriter from "typewriter-effect";
 
 import classes from "./landinginfo.module.css";
 
@@ -7,7 +8,16 @@ const LandingInfo = () => {
   return (
     <div>
       <h1 className={classes.title}>
-        Hello, I'm <span className={classes.name}>Shane</span> 👋🏻 🇬🇧
+        <p>Hello, I'm <span className={classes.name}>Shane</span> 👋🏻 🇬🇧</p>
+        <div>
+          <Typewriter 
+              options={{
+                strings: ["Technical Support Engineer", "Software Engineer","Front End Engineer"],
+                autoStart: true,
+                loop: true
+              }}
+          />
+        </div>
       </h1>
       <div className={classes.socials}>
         <a href="https://github.com/Chaffexd" target="_blank">
